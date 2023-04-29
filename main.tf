473983861b11425fef68752f004e41dd29593054
OdinValholl <sahin-onur@t-online.de>
2023-04-29 17:23:08 +0200

    Lint

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "random_password" "admin_password" {
   length           = 16
   special          = true
   override_special = "_@%"
-  numeric           = true
+  numeric          = true
 }
 
 # Create resource group
